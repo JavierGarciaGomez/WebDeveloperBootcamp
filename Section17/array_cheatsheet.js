b32aafd2cef4a48a626bb9ea486b15010db0f25b
JGG <javieron.garcia@gmail.com>
2020-11-18 16:29:44 +0100

    Section 17

---
+++ Section17/array_cheatsheet.js
@@ -0,0 +1,37 @@
+// Making an array:
+const colors = ["red", "orange", "yellow"];
+
+// Arrays are indexed like strings:
+colors[0]; // "red"
+
+// They have a length:
+colors.length; //3
+
+// Important array methods:
+//push(value) - adds value to the END of an array
+//pop() - removes and returns last value in array
+
+//unshift(val) - adds value to START of an array
+//shift() - removes and returns first element in an array
+
+
+
+// Exercise 25
+let lottoNumbers = [1, 2, 3, 4, 5, 6]
+
+// exercise 26
+
+leaderboard[1] = 'Luna';
+leaderboard[3] = 'Draco';
+
+// exercise 27
+const planets = ['The Moon', 'Venus', 'Earth', 'Mars', 'Jupiter']; //DO NOT TOUCH THIS LINE!
+
+// YOUR CODE GOES BELOW THIS LINE:
+planets.unshift("The Moon");
+planets.push("Saturn");
+
+
+// exercise 28
+
+airplaneSeats[3][1] = "Hugo"
