b189d5a3486fbc3a5ca81d6da0d43d87b5bb9811
JGG <javieron.garcia@gmail.com>
2020-11-19 22:27:18 +0100

    Section 19

---
+++ Section19/app.js
@@ -0,0 +1,291 @@
+
+// 191
+
+// ======================================
+// WITHOUT ANY LOOPS...LOTS OF REPETITION
+// ======================================
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+// console.log(6)
+// console.log(7)
+// console.log(8)
+// console.log(9)
+// console.log(10)
+
+// ======================================
+// OUR FIRST FOR LOOP
+// ======================================
+
+console.log("191. Our first loop")
+for (let num = 1; num <= 10; num += 1) {
+    console.log(num);
+}
+
+for (let num2 = 99; num2 >= 90; num2 -= 3) {
+    console.log("Printing a number " + num2);
+}
+
+// ======================================
+// MORE EXAMPLES OF FOR LOOPS!
+// ======================================
+
+// for (let i = 1; i <= 20; i += 2) {
+//     console.log(i)
+// }
+
+// for (let i = 100; i >= 0; i -= 10) {
+//     console.log(i);
+// }
+
+// for (let i = 10; i <= 1000; i *= 10) {
+//     console.log(i);
+// }
+
+// exercise 31
+// Print out "Da ba dee da ba daa" 6 times, using a for loop
+for (let i = 0; i < 6; i++) {
+    console.log("Da ba dee da ba daa");
+}
+
+
+// exercise 32
+// Write a loop that prints:
+// 25
+// 20
+// 15
+// 10
+// 5
+// 0
+
+for (let i = 25; i >= 0; i -= 5) {
+    console.log(i)
+}
+
+// 193
+// DO NOT RUN THIS LOOP
+// INFINITE LOOP!! BAD!!
+// ================================
+// ================================
+// for (let i = 20; i >= 0; i++) {
+//     console.log(i)
+// }
+// ================================
+// ================================
+
+// 194 for loop
+const animals = ["Bear"];
+
+for (let i = 0; i < animals.length; i++) {
+    console.log(i, animals[i]);
+}
+
+// Exercise 33
+const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"]; //DONT TOUCH THIS LINE!
+
+// WRITE YOUR LOOP BELOW THIS LINE:
+for (let i = 0; i < people.length; i++) {
+    console.log(people[i].toUpperCase());
+}
+
+//195
+
+// ===================================
+// NESTED FOR LOOP EXAMPLES
+// ===================================
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`i is: ${i}`)
+    for (let j = 1; j < 4; j++) {
+        console.log(`     j is: ${j}`)
+    }
+}
+
+// ===================================
+// NESTED ARRAY ITERATION
+// ===================================
+
+
+const seatingChart = [
+    ['Kristen', 'Erik', 'Namita'],
+    ['Geoffrey', 'Juanita', 'Antonio', 'Kevin'],
+    ['Yuma', 'Sakura', 'Jack', 'Erika']
+]
+
+for (let i = 0; i < seatingChart.length; i++) {
+    const row = seatingChart[i];
+    console.log(`ROW #${i + 1}`)
+    for (let j = 0; j < row.length; j++) {
+        console.log(row[j])
+    }
+}
+
+
+// 196
+
+// let count = 0;
+// while (count < 10) {
+//     count++;
+//     console.log(count)
+// }
+
+// const SECRET = "BabyHippo";
+
+// let guess = prompt("enter the secret code...");
+// while (guess !== SECRET) {
+//     guess = prompt("enter the secret code...");
+// }
+// console.log("CONGRATS YOU GOT THE SECRET!!!")
+
+// 197
+
+// let input = prompt("Hey, say something!")
+// while (true) {
+//     input = prompt(input);
+//     if (input.toLowerCase() === "stop copying me") break;
+// }
+// console.log("OK YOU WIN!")
+
+// for (let i = 0; i < 1000; i++) {
+//     console.log(i);
+//     if (i === 100) break;
+// }
+
+let maximum = parseInt(prompt("Enter the maximum number!"));
+while (!maximum) {
+    maximum = parseInt(prompt("Enter a valid number!"));
+}
+
+const targetNum = Math.floor(Math.random() * maximum) + 1;
+
+let guess = parseInt(prompt("Enter your first guess!"));
+let attempts = 1;
+
+while (parseInt(guess) !== targetNum) {
+    if (guess === 'q') break;
+    attempts++;
+    if (guess > targetNum) {
+        guess = prompt("Too high! Enter a new guess:");
+    } else {
+        guess = prompt("Too low! Enter a new guess:");
+    }
+}
+
+if (guess === 'q') {
+    console.log("OK, YOU QUIT!")
+} else {
+    console.log("CONGRATS YOU WIN!")
+    console.log(`You got it! It took you ${attempts} guesses`)
+}
+
+
+// 199 FOR OF
+
+const subreddits = ['cringe', 'books', 'chickens', 'funny', 'pics', 'soccer', 'gunners'];
+
+// for (let i = 0; i < subreddits.length; i++) {
+//     console.log(`Visit reddit.com/r/${subreddits[i]}`)
+// }
+
+for (let subreddit of subreddits) {
+    console.log(`Visit reddit.com/r/${subreddit}`)
+}
+
+
+
+// const seatingChart = [
+//     ['Kristen', 'Erik', 'Namita'],
+//     ['Geoffrey', 'Juanita', 'Antonio', 'Kevin'],
+//     ['Yuma', 'Sakura', 'Jack', 'Erika']
+// ]
+
+// for (let i = 0; i < seatingChart.length; i++) {
+//     const row = seatingChart[i];
+//     for (let j = 0; j < row.length; j++) {
+//         console.log(row[j])
+//     }
+// }
+
+// for (let row of seatingChart) {
+//     for (let student of row) {
+//         console.log(student);
+//     }
+// }
+
+// for (let char of "hello world") {
+//     console.log(char)
+// }
+
+// Exercise 34
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]; //DON'T CHANGE THIS LINE PLEASE!
+
+// WRITE YOUR LOOP BELOW THIS LINE:
+for (let num of numbers) {
+
+    console.log(Math.pow(num, 2));
+
+}
+
+// 200
+
+const testScores = {
+    keenan: 80,
+    damon: 67,
+    kim: 89,
+    shawn: 91,
+    marlon: 72,
+    dwayne: 77,
+    nadia: 83,
+    elvira: 97,
+    diedre: 81,
+    vonnie: 60
+}
+// ===================================
+// USING FOR...IN TO ITERATE AN OBJECT
+// ===================================
+
+for (let person in testScores) {
+    console.log(`${person} scored ${testScores[person]}`);
+}
+
+// ==============================================
+// Using Object methods to iterate
+// (turn data into an array and then use for...of)
+// ==============================================
+
+let total = 0;
+let scores = Object.values(testScores);
+for (let score of scores) {
+    total += score;
+}
+console.log(total / scores.length)
+
+//201 Todo List
+
+let input = prompt('what would you like to do?');
+const todos = ['Collect Chicken Eggs', 'Clean Litter Box'];
+while (input !== 'quit' && input !== 'q') {
+    if (input === 'list') {
+        console.log('*****************')
+        for (let i = 0; i < todos.length; i++) {
+            console.log(`${i}: ${todos[i]}`);
+        }
+        console.log('*****************')
+    } else if (input === 'new') {
+        const newTodo = prompt('Ok, what is the new todo?');
+        todos.push(newTodo);
+        console.log(`${newTodo} added to the list!`)
+    } else if (input === 'delete') {
+        const index = parseInt(prompt('Ok, enter an index to delete:'));
+        if (!Number.isNaN(index)) {
+            const deleted = todos.splice(index, 1);
+            console.log(`Ok, deleted ${deleted[0]}`);
+        } else {
+            console.log('Unknown index')
+        }
+    }
+    input = prompt('what would you like to do?')
+}
+console.log('OK QUIT THE APP!')
