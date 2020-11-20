c654c84e47aec2dc74d64cd2eb5774c11ef11924
JGG <javieron.garcia@gmail.com>
2020-11-20 10:46:15 +0100

    Section 21

--- Section20/app.js
+++ Section20/app.js
@@ -39,3 +39,80 @@ function add(x, y) {
 // singSong()
 // singSong()
 
+// Ex 35
+function printHeart() {
+    console.log("<3");
+}
+
+// Ex 36
+
+function rant(message) {
+    for (let i = 0; i < 3; i++) {
+        console.log(message.toUpperCase());
+    }
+}
+
+// Ex 37
+
+function isSnakeEyes(num1, num2) {
+    if (num1 == 1 && num1 == num2) {
+        console.log("Snake Eyes!");
+    } else {
+        console.log("Not Snake Eyes!");
+    }
+}
+
+// Ex 38
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+// Ex 39
+
+function isShortsWeather(temperature) {
+    if (temperature >= 75) {
+        return true;
+    }
+    else return false;
+}
+
+// Ex40
+function lastElement(arr) {
+    if (arr.length) {
+        return arr[arr.length - 1];
+    }
+    return null;
+}
+
+// Ex41
+function capitalize(str) {
+    return str[0].toUpperCase() + str.slice(1);
+}
+
+//42
+function sumArray(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum = sum + arr[i];
+    }
+    return sum;
+}
+
+//43
+const days = {
+    1: 'Monday',
+    2: "Tuesday",
+    3: "Wednesday",
+    4: "Thursday",
+    5: "Friday",
+    6: "Saturday",
+    7: "Sunday"
+}
+
+const returnDay = (number) => {
+    if (number < 1 || number > 7) {
+        return null;
+    }
+    return (days[number])
+}
---
+++ Section21/app.js
@@ -0,0 +1,128 @@
+// ==============
+// 210 FUNCTION SCOPE
+// ==============
+let totalEggs = 0;
+function collectEggs() {
+    totalEggs = 6;
+}
+console.log(totalEggs);
+collectEggs();
+console.log(totalEggs);
+
+const bird = 'Scarlet Macaw';
+function birdWatch() {
+    const bird = 'Great Blue Heron';
+    console.log(bird);
+}
+birdWatch()
+
+// ==============
+// 211 BLOCK SCOPE
+// ==============
+// let radius = 8;
+// if (radius > 0) {
+//     const PI = 3.14159;
+//     let msg = 'HIII!'
+// }
+// console.log(radius);
+// console.log(msg)
+
+// for (let i = 0; i < 5; i++) {
+//     let msg = "ASKLDJAKLSJD";
+//     console.log(msg)
+// }
+// console.log(msg)
+// console.log(i)
+
+// ==============
+// 212 LEXICAL SCOPE
+// ==============
+
+function bankRobbery() {
+    const heroes = ['Spiderman', 'Wolverine', 'Black Panther', 'Batwoman']
+    function cryForHelp() {
+        let color = 'purple';
+        function inner() {
+            for (let hero of heroes) {
+                console.log(`PLEASE HELP US, ${hero.toUpperCase()}`)
+            }
+        }
+        inner();
+    }
+    cryForHelp();
+}
+
+
+// 213 Function expressions
+
+// function add(x, y) {
+//     return x + y;
+// }
+
+const add = function (x, y) {
+    return x + y;
+}
+
+// Ex 44
+//NOTE: Udemy's coding exercise platform does NOT support the ** operator.  You'll need to multiply a number by itself or use the Math.pow() method.
+
+const square = function (num) {
+    return Math.pow(num, 2);
+}
+
+// ======================
+// 214 FUNCTIONS AS ARGUMENTS
+// ======================
+
+console.log(214);
+function callTwice(func) {
+    func();
+    func();
+}
+
+function callTenTimes(f) {
+    for (let i = 0; i < 10; i++) {
+        f()
+    }
+}
+
+function rollDie() {
+    const roll = Math.floor(Math.random() * 6) + 1
+    console.log(roll)
+}
+
+console.log("Im calling call Twice(rollDie)")
+callTwice(rollDie)
+
+// ====================
+// 215 RETURNING FUNCTIONS
+// ====================
+
+function makeMysteryFunc() {
+    const rand = Math.random();
+    if (rand > 0.5) {
+        return function () {
+            console.log("CONGRATS, I AM A GOOD FUNCTION!")
+            console.log("YOU WIN A MILLION DOLLARS!!")
+        }
+    } else {
+        return function () {
+            alert("YOU HAVE BEEN INFECTED BY A COMPUTER VIRUS!")
+            alert("STOP TRYING TO CLOSE THIS WINDOW!")
+            alert("STOP TRYING TO CLOSE THIS WINDOW!")
+            alert("STOP TRYING TO CLOSE THIS WINDOW!")
+            alert("STOP TRYING TO CLOSE THIS WINDOW!")
+            alert("STOP TRYING TO CLOSE THIS WINDOW!")
+        }
+    }
+}
+
+
+function makeBetweenFunc(min, max) {
+    return function (num) {
+        return num >= min && num <= max;
+    }
+}
+
+
+
