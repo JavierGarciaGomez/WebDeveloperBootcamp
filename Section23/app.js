9e0bd63793f5803822c002a395b2ec23f73868de
JGG <javieron.garcia@gmail.com>
2020-11-20 13:47:38 +0100

    Section 23

---
+++ Section23/app.js
@@ -0,0 +1,56 @@
+// ==========================================
+// 232. Deafault params
+// ==========================================
+
+// ==========================================
+// AN OLDER WAY OF ADDING DEFAULT PARAM VALUE
+// ==========================================
+
+// function rollDie(numSides) {
+//     if (numSides === undefined) {
+//         numSides = 6
+//     }
+//     return Math.floor(Math.random() * numSides) + 1
+// }
+
+// ============
+// THE NEW WAY!
+// ============
+function rollDie(numSides = 6) {
+    return Math.floor(Math.random() * numSides) + 1
+}
+
+function greet(person, msg = "Hey there", punc = '!') {
+    console.log(`${msg}, ${person}${punc}`)
+}
+
+// ==========================================
+// 233. Spread
+// ==========================================
+const nums = [13, 4, 5, 21, 3, 3, 1, 2, 7, 6, 4, 2, 53456];
+// SPREAD IN FUNCTIONS
+Math.max(...nums) //NaN
+Math.max(nums) //53456
+
+
+// SPREAD IN ARRAYS
+const cats = ['Blue', 'Scout', 'Rocket'];
+const dogs = ['Rusty', 'Wyatt'];
+
+const allPets = [...cats, ...dogs];
+
+
+// SPREAD IN OBJECTS
+const feline = { legs: 4, family: 'Felidae' };
+const canine = { isFurry: true, family: 'Caninae' };
+
+const catDog = { ...feline, ...canine };
+
+
+const dataFromForm = {
+    email: 'blueman@gmail.com',
+    password: 'tobias123!',
+    username: 'tfunke'
+}
+const newUser = { ...dataFromForm, id: 2345, isAdmin: false }
+
