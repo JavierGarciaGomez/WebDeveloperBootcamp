7c0426b3cfbcb39f250251bcfb873e70d5fc6b07
JGG <javieron.garcia@gmail.com>
2020-11-30 13:08:14 +0100

    Section 29

--- Section29/app.js
+++ Section29/app.js
@@ -16,3 +16,121 @@ Array.prototype.pop = function () {
 };
 const nums = [6, 7, 8, 9, 10];
 nums.pop(); // "SORRY I WANT THAT ELEMENT, I WILL NEVER POP IT OFF!"
+
+///////////////////////////////
+// Lesson 295
+//////////////////////////////
+
+//This functions makes and returns an object every time it is called.
+// The resulting objects all follow the same "recipe"
+function makeColor(r, g, b) {
+	const color = {};
+	color.r = r;
+	color.g = g;
+	color.b = b;
+	color.rgb = function () {
+		const { r, g, b } = this;
+		return `rgb(${r}, ${g}, ${b})`;
+	};
+	color.hex = function () {
+		const { r, g, b } = this;
+		return (
+			'#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
+		);
+	};
+	return color;
+}
+
+const firstColor = makeColor(35, 255, 150);
+firstColor.hex(); //firstColor.hex();
+firstColor.rgb(); //"rgb(35, 255, 150)"
+
+const black = makeColor(0, 0, 0);
+black.rgb(); //"rgb(0, 0, 0)"
+black.hex(); //"#0000s00"
+
+console.log(firstColor.hex())
+console.log(firstColor.rgb())
+
+firstColor.r = 255
+console.log(firstColor.hex())
+console.log(firstColor.rgb())
+
+///////////////////////////////
+// Lesson 296
+//////////////////////////////
+console.log("******************Lesson 296*************")
+
+// This is a Constructor Function...
+function Color(r, g, b) {
+	this.r = r;
+	this.g = g;
+	this.b = b;
+}
+
+//If you call it on its own like a regular function...
+Color(35, 60, 190); //undefined
+//It returns undefined. Seems useless!
+
+// *****************
+// THE NEW OPERATOR!
+// *****************
+
+// 1. Creates a blank, plain JavaScript object;
+// 2. Links (sets the constructor of) this object to another object;
+// 3. Passes the newly created object from Step 1 as the this context;
+// 4. Returns this if the function doesn't return its own object.
+
+Color.prototype.rgb = function () {
+	const { r, g, b } = this;
+	return `rgb(${r}, ${g}, ${b})`;
+};
+
+Color.prototype.hex = function () {
+	const { r, g, b } = this;
+	return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+};
+
+Color.prototype.rgba = function (a = 1.0) {
+	const { r, g, b } = this;
+	return `rgba(${r}, ${g}, ${b}, ${a})`;
+};
+
+const color1 = new Color(40, 255, 60);
+color1.hex();
+const color2 = new Color(0, 0, 0);
+color2.hex();
+console.log(color1)
+
+///////////////////////////////
+// Lesson 297
+//////////////////////////////
+console.log("******************Lesson 297*************")
+
+class Colorb {
+	constructor(r, g, b, name) {
+		this.r = r;
+		this.g = g;
+		this.b = b;
+		this.name = name;
+	}
+	innerRGB() {
+		const { r, g, b } = this;
+		return `${r}, ${g}, ${b}`;
+	}
+	rgb() {
+		return `rgb(${this.innerRGB()})`;
+	}
+	rgba(a = 1.0) {
+		return `rgba(${this.innerRGB()}, ${a})`;
+	}
+	hex() {
+		const { r, g, b } = this;
+		return (
+			'#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
+		);
+	}
+}
+const red = new Colorb(255, 67, 89, 'tomato');
+const white = new Colorb(255, 255, 255, 'white');
+console.log(red)
