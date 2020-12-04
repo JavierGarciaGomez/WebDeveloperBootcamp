6bbd8e94c93cbe7a06305798dce7c2abfa348754
JGG <javieron.garcia@gmail.com>
2020-12-04 01:51:41 +0100

    Section 40

---
+++ Section40/Middleware_Intro/index418.js
@@ -0,0 +1,47 @@
+// 415
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+// 415
+app.use(morgan('tiny'));
+
+// 416
+// app.use((req, res, next) => {
+//     res.send('Hijacked')
+//     next();
+// })
+
+// 417
+app.use((req, res, next) => {
+    req.requestTime = Date.now();
+    console.log(req.method.toUpperCase(), req.path)
+    next();
+})
+
+// 418
+app.use('/dogs', (req, res, next) => {
+    console.log("I LOVE DOGS!!")
+    next();
+})
+
+// 415
+app.get('/', (req, res) => {
+    console.log(`REQUEST DATE: ${req.requestTime}`)
+    res.send('HOME PAGE!')
+})
+
+app.get('/dogs', (req, res) => {
+    console.log(`REQUEST DATE: ${req.requestTime}`)
+    res.send('WOOF WOOF!')
+})
+
+// 418
+app.use((req, res) => {
+    res.status(404).send('NOT FOUND!')
+})
+
+// 415
+app.listen(3000, () => {
+    console.log('App is running on localhost:3000')
+})
